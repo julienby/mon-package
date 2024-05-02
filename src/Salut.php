0b8cbecc2fdc4e8e1d8a43e913b1debb41f0b118
Julien BY <julien.baudry@gmail.com>
2024-05-02 17:37:26 +0200

    new function

--- src/Salut.php
+++ src/Salut.php
@@ -6,5 +6,9 @@ class Salut {
     public static function direBonjour() {
         return "Bonjour tout le monde !";
     }
+    
+    public function hello() {
+        return "Hello tout le monde !";
+    }
 }
 
